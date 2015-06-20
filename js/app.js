da585ca451e96b59ecf0baf5c05d69f0dc2780ea
Guillaume Ialiprete <guillaume.ialiprete@gmail.com>
2015-06-20 14:06:29 +0200

    ye

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ app.config(function($mdThemingProvider, $mdIconProvider){
 		.defaultIconSet("./images/svg/avatars.svg", 128)
 		.icon("menu"       , "./images/svg/menu.svg"        , 24)
 		.icon("share"      , "./images/svg/share.svg"       , 24)
+		.icon("settings"   , "./images/svg/settings.svg"    , 24)
+		.icon("home"       , "./images/svg/home.svg"        , 24)
 		.icon("google_plus", "./images/svg/google_plus.svg" , 512)
 		.icon("hangouts"   , "./images/svg/hangouts.svg"    , 512)
-		.icon("twitter"    , "./images/svg/twitter.svg"     , 512)
 		.icon("phone"      , "./images/svg/phone.svg"       , 512);
 
 	$mdThemingProvider.theme('default')
@@ -16,13 +17,22 @@ app.config(function($mdThemingProvider, $mdIconProvider){
 });
 
 app.config(function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'templates/home.html',
-		controller: 'HomeController'
-	})
-	.otherwise({redirectTo: '/'})
+	$routeProvider
+		.when('/', {
+			templateUrl: 'templates/home.html',
+			controller: 'HomeController'
+		})
+		.when('/settings', {
+			templateUrl: 'templates/settings.html',
+			controller: 'SettingsController'
+		})
+		.otherwise({redirectTo: '/'})
 });
 
 app.controller('HomeController', function() {
+	
+})
+
+app.controller('SettingsController', function() {
 	
 })
